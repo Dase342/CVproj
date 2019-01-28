5e657ff7d53688230131728ea164ca21b3557976
david to <davidt_888@hotmail.com>
2019-01-28 14:18:14 -0500

    Ajout des variables

---
+++ CV.java
@@ -0,0 +1,16 @@
+
+public class CV {
+	
+	private String nom;
+	
+	private String prenom;
+	
+	private String formation;
+	
+	private int experience;
+	
+	private String[] competence;
+	
+	private String attente;
+
+}
